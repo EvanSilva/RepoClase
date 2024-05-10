1143933ab3306823964210175d080801dffb246a
GermanRM <46497145+Patriciagpardo@users.noreply.github.com>
2024-05-10 10:25:06 +0200

    PrimerCommit NetBeans Ej 4.10 dos

---
+++ src/main/java/EjercicioGit/Sumador.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package EjercicioGit;
+
+/**
+ *
+ * @author paty
+ */
+
+public class Sumador {
+public static float sumar (float a, float b ) {
+return a + b;
+}}
